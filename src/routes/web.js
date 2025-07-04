324fa13faeb2f6a8bcee878834b1235815cb4881
Chuong Cao <chuongb2105536@student.ctu.edu.vn>
2025-07-05 02:13:23 +0700

    Test API

--- src/routes/web.js
+++ src/routes/web.js
@@ -6,6 +6,7 @@ const { getHomepage,
         getEditUserPage,
         handleUpdateUser,
         handleDeleteUser } = require('../controllers/homeController');
+const { testApi } = require('../controllers/apiController');
 const router = express.Router();
 
 // router.Method('/route', handler)
@@ -17,5 +18,8 @@ router.get('/edit-user', getEditUserPage);
 router.post('/create-user', handleCreateNewUser);
 router.post('/update-user', handleUpdateUser);
 router.get('/delete-user', handleDeleteUser);
+
+// Restful API
+router.get('/api/test-api', testApi);
 
 module.exports = router;
