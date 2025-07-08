ebbf9ad4cddabccac04a029e63f9a520a3ba2d5e
Chuong Cao <chuongb2105536@student.ctu.edu.vn>
2025-07-08 18:14:02 +0700

    API xoa nguoi dung

--- src/services/userApiService.js
+++ src/services/userApiService.js
@@ -35,7 +35,9 @@ const layNguoiDungTheoTrang = async (page, limit) => {
         let offset = (page - 1) * limit;
         const { count, rows } = await db.NguoiDung.findAndCountAll({
             offset: offset,
-            limit: limit
+            limit: limit,
+            attributes: ["id", "soDienThoai", "hoTen", "email", "soDD", "gioiTinh", "ngaySinh", "dcThuongTru"],
+            include: { model: db.NhomND, attributes: ["tenNhom"] }
         });
         let totalPages = Math.ceil(count / limit);
         let data = {
@@ -96,11 +98,32 @@ const capNhatTTNguoiDung = async (data) => {
 
 const xoaNguoiDungBangId = async (userId) => {
     try {
-        await db.NguoiDung.delete({
+        let nguoiDung = await db.NguoiDung.findOne({
             where: {id: userId}
-        })
+        });
+
+        if (nguoiDung) {
+            await nguoiDung.destroy();
+
+            return {
+                EM: 'Xóa người dùng thành công! (User deleted successfully)',
+                EC: 0,
+                DT: []
+            };
+        } else {
+            return {
+                EM: 'Người dùng không tồn tại. (User does not exited)',
+                EC: 2,
+                DT: []
+            };
+        }
     } catch (e) {
         console.log(e);
+        return {
+            EM: 'Có gì đó không đúng! (Something went wrong in service)',
+            EC: 1,
+            DT: []
+        };
     }
 }
 
