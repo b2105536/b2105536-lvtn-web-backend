cd21f7bc857bbcc3be33a20622ba02898391bc16
Chuong Cao <chuongb2105536@student.ctu.edu.vn>
2025-07-11 17:23:41 +0700

    Kiem tra quyen han nguoi dung

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,25 +1,18 @@
 const express = require('express');
-const { testApi,
-        handleRegister,
-        handleLogin } = require('../controllers/apiController');
+const { handleRegister, handleLogin } = require('../controllers/apiController');
 const { readFunc,
         createFunc,
         updateFunc,
         deleteFunc } = require('../controllers/userController');
 const { readFunc: groupReadFunc } = require('../controllers/groupController');
+const { checkUserJWT, checkUserPermission } = require('../middleware/JWTAction');
 const router = express.Router();
 
-const testMiddleware = (req, res, next) => {
-    console.log("Calling a middleware!");
-    next();
-}
-
 // Restful API('/route', api)
-router.get('/test-api', testApi);
 router.post('/register', handleRegister);
 router.post('/login', handleLogin);
 
-router.get('/user/read', readFunc);
+router.get('/user/read', checkUserJWT, checkUserPermission, readFunc);
 router.post('/user/create', createFunc);
 router.put('/user/update', updateFunc);
 router.delete('/user/delete', deleteFunc);
